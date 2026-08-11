9503494384c3eab849131aed2c026bbcb181a113
Sheldon Ombaso <55881806+sheldonombaso@users.noreply.github.com>
2026-08-11 22:53:43 +0300

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,44 @@
+--IRI DATABASE
 
+/*
+=============================================================
+Create Database and Schemas
+=============================================================
+Script Purpose:
+    This script creates a new database named 'IRIDataWarehouse' after checking if it already exists. 
+    If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas 
+    within the database: 'bronze', 'silver', and 'gold'.
+	
+WARNING:
+    Running this script will drop the entire 'IRIDataWarehouse' database if it exists. 
+    All data in the database will be permanently deleted. Proceed with caution 
+    and ensure you have proper backups before running this script.
+*/
+
+USE master;
+GO
+
+-- Drop and recreate the 'DataWarehouse' database
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'IRIDataWarehouse')
+BEGIN
+    ALTER DATABASE IRIDataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE IRIDataWarehouse;
+END;
+GO
+
+-- Create the 'IRIDataWarehouse' database
+CREATE DATABASE IRIDataWarehouse;
+GO
+
+USE IRIDataWarehouse;
+GO
+
+-- Create Schemas
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
